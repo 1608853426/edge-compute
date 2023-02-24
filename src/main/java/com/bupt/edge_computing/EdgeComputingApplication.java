f258140ddab4137a0bc8fd35d115035fe150ebd6
SoonMachine <sunamnxin@126.com>
2023-02-24 13:41:52 +0800

    create repo

---
+++ src/main/java/com/bupt/edge_computing/EdgeComputingApplication.java
@@ -0,0 +1,13 @@
+package com.bupt.edge_computing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeComputingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeComputingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bupt/edge_computing/EdgeComputingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bupt.edge_computing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeComputingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
